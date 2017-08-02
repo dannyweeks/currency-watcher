bdacbe621579066066cdd788326742548214cef2
Danny Weeks <danny@dannyweeks.com>
2017-08-02 23:59:21 +0100

    fix doctrine

--- cli-config.php
+++ cli-config.php
@@ -1,6 +1,7 @@
 <?php
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
 require_once 'bootstrap.php';
 
-return ConsoleRunner::createHelperSet($app->getContainer()->get('em'));
+return ConsoleRunner::createHelperSet($app->getContainer()->get(EntityManager::class));
